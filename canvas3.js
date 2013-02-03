5943e8fc8a12c2d2275a2a40adb5c1e8b30b468d
Anton Sigurðsson <anton.sigurdsson@gmail.com>
2013-02-03 22:59:50 +0000

    comment og jslint

--- canvas3.js
+++ canvas3.js
@@ -1,25 +1,30 @@
-////////////////// Verkefni í Vefforritun2 HR - HTML5 Canvas ////////////////
+
+// Verkefni í Vefforritun2 HR - HTML5 Canvas  //
 //  Nemendur:
 //   Anton Sigurðsson - antons11@ru.is
 //   Sigurður Nónsson - sigurdurjon11@ru.is
-/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////
 
 ////////////////////////////////////////////////
 //  GLOBAL BREYTUR
 ////////////////////////////////////////////////
+
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
-var mouseDown = false;
+
 var shapeArray = [];
 var undoRedoArray = [];
 var shapeArraylength = -1;
 var undoRedoArraylength = 0;
+
 var form = "brush";
 var color = "rgba(0,0,0,1)";
 var fyllir = false;
 var fontur = 'arial';
 var texti = '';
 var lineWidth = 6;
+
+var mouseDown = false;
 var mouseX = 0;
 var mouseY = 0;
 var mousedownX = 0;
@@ -35,13 +40,9 @@ var isclean = false;
 //  Teiknar aftur öll klasaeintök á canvasinn.
 function redraw() {
  context.clearRect(0, 0, 800, 400);
-
+ 
  for(var i = 0; i < shapeArraylength + 1; i++) {		
   shapeArray[i].draw(context);
-  console.log(shapeArray[i].x + " " + shapeArray[i].y + " " 
-  + shapeArray[i].endx + " " + shapeArray[i].endy + " " 
-  + shapeArray[i].lineW + " " + shapeArray[i].col + " " 
-  + shapeArray[i].shapeName);
  }
 }
 
@@ -51,7 +52,7 @@ function undo() {
  if(isclean){
   var temp = undoRedoArraylength;
   for(var i = 0; i < temp + 1; i++) {
-  redo();
+   redo();
   }
  }
  else{
@@ -71,7 +72,7 @@ function undo() {
 }
 
 //  Sækir síðasta undo-aða klasaeintak á canvasinn
-function redo(){
+function redo() {
  if(undoRedoArraylength > 0) {
   undoRedoArraylength--;
   shapeArraylength++;
@@ -83,7 +84,7 @@ function redo(){
 //  Hreinsar canvasinn með því að setja öll klasaeintök í undo
 function hreinsa() {
  var len = shapeArraylength;
- for (var i = 0; i < len +1; i++){
+ for (var i = 0; i < len +1; i++) {
   undo();			
  }
  isclean = true;
@@ -96,7 +97,7 @@ getMouse = function(e, canvas) {
   do{
    offsetX += element.offsetLeft;
    offsetY += element.offsetTop;
-  }while ((element = element.offsetParent));
+  } while ((element = element.offsetParent));
  }
  return {
   x: e.pageX - offsetX,
@@ -105,12 +106,12 @@ getMouse = function(e, canvas) {
 }
 
 //  Uppfærir valinn lit frá vefsíðu
-function selcol(c){
+function selcol(c) {
  color = c;
 }
 
 //  Uppfærir valið form frá vefsíðu
-function selform(newForm){
+function selform(newForm) {
  form = newForm;
 }
 
@@ -129,273 +130,268 @@ $('#submit').click(function() {
  var uploadcontext = uploadcanvas.getContext('2d');
  var img = new Image()
  img.src = c
- uploadcontext.drawImage(img,0,0);
- if(a.value === ""){
+ uploadcontext.drawImage(img, 0, 0);
+ if(a.value === "") {
   alert('No image selected, press "Choose File" to select an image!');
  }
 });
 
-//  Sækir úr checkboxi hvort teikna eigi fylltan hlut
-$('#check').click( function(){
- if(document.getElementById('check').checked){
+//  Sækir úr checkboxi hvort teikna eigi fylltan hlut.
+$('#check').click( function() {
+ if(document.getElementById('check').checked) {
   fyllir = true;
  }
- else{
+ else {
   fyllir = false;
  }
 });
 
-//  Mousedown skilar
-$('#myCanvas').mousemove(function (e){
-	if(mouseDown){
-		var tempMouse = getMouse(e, canvas);
-		mouseX = tempMouse.x;
-		mouseY = tempMouse.y;
-		shapeArray[shapeArraylength].endx = mouseX;
-		shapeArray[shapeArraylength].endy = mouseY;
-		if(form === 'brush'){
-			shapeArray[shapeArraylength].hnitx.push(mouseX);
-			shapeArray[shapeArraylength].hnity.push(mouseY);
-		}
-		if(form === 'circle'){
-			shapeArray[shapeArraylength].savecircle();
-		}
-		
-		redraw();
-    }
+// Mousedown býr til nýtt klasaeintak og sendir inní smiðinn 
+// gildum sem koma úr globalbreytum, stöðu músar og af síðunni.
+$('#myCanvas').mousedown(function (e) {
+ if(undoRedoArraylength > 0){
+  undoRedoArraylength = 0;
+ }
+ 
+ shapeArraylength++;
+ var tempMouse = getMouse(e, canvas);
+ mousedownX = tempMouse.x;
+ mousedownY = tempMouse.y;
+ lineWidth = document.getElementById('linewidth').value;
+ fontur = document.getElementById('fontval').value;
+ texti = document.getElementById('textareitur').value;
+
+ var newShape = createShapeClass(mousedownX, mousedownY, color, lineWidth, form, fyllir, fontur, texti);
+
+ shapeArray.push(newShape);
+ mouseDown = true;
+});
+
+//  Mousemove fallið er eingöngu virkt þegar mousedown er true.
+//  Það skrifar í klasaeintökin endahnitin til að hægt sé 
+//  að teikna þau áður en mús er sleppt.
+$('#myCanvas').mousemove(function (e) {
+ if(mouseDown) {
+  var tempMouse = getMouse(e, canvas);
+  mouseX = tempMouse.x;
+  mouseY = tempMouse.y;
+  shapeArray[shapeArraylength].endx = mouseX;
+  shapeArray[shapeArraylength].endy = mouseY;
+  if(form === 'brush') {
+   shapeArray[shapeArraylength].hnitx.push(mouseX);
+   shapeArray[shapeArraylength].hnity.push(mouseY);
+  }
+  if(form === 'circle') {
+   shapeArray[shapeArraylength].savecircle();
+  }
+ redraw();
+ }
 });	
 
-$('#myCanvas').mousedown(function (e){
-	if(undoRedoArraylength > 0){
-		undoRedoArraylength = 0;
-	}
-
-	shapeArraylength++;		
-	var tempMouse = getMouse(e, canvas);
-	mousedownX = tempMouse.x;
-	mousedownY = tempMouse.y;
-	lineWidth = document.getElementById('linewidth').value;
-	fontur = document.getElementById('fontval').value;
-	texti = document.getElementById('textareitur').value;
-	var newShape = createShapeClass(mousedownX, mousedownY, color, 
-	lineWidth, form, fyllir, fontur, texti);
-    console.log("MX " + mousedownX + "| MY " + mousedownY + "| Color " 
-	+ color + "| lineWidth " + lineWidth  + "| nameofShape " + form 
-	+ "| Filled " + fyllir);
-    console.log(newShape.shapeName);
-	shapeArray.push(newShape);
-	mouseDown = true;
+//  Mouseup vistar endamúsa gildin.
+$('#myCanvas').mouseup(function (e) {
+ var tempMouse = getMouse(e, canvas);
+ mouseX = tempMouse.x;
+ mouseY = tempMouse.y;
+ mouseDown = false;
+ shapeArray[shapeArraylength].endx = mouseX;
+ shapeArray[shapeArraylength].endy = mouseY;
+ shapeArray[shapeArraylength].draw(context);
 });
 
-
-$('#myCanvas').mouseup(function (e){	
-	mouseDown = false;
-	var tempMouse = getMouse(e, canvas);
-	mouseX = tempMouse.x;
-	mouseY = tempMouse.y;
-	shapeArray[shapeArraylength].endx = mouseX;
-	shapeArray[shapeArraylength].endy = mouseY;
-	shapeArray[shapeArraylength].draw(context);
-});
-
-function createShapeClass(x, y, col, lw, sh, filled, fo, tex){
-	if(sh === 'brush'){
-		return new Brush(x, y, col, lw, sh, filled);
-	}
-	else if(sh === 'line'){
-		return new Line(x, y, col, lw, sh, filled);
-	}
-	else if(sh === 'square'){
-		return new Square(x, y, col, lw, sh, filled);
-	}
-	else if(sh === 'circle'){
-		return new Circle(x, y, col, lw, sh, filled);
-	}
-	else if(sh === 'triangle'){
-		return new Triangle(x, y, col, lw, sh, filled);
-	}
-	else if(sh === 'text'){
-		return new Texti(x, y, col, lw, sh, filled, fo, tex);
-	}
+//  Createshape returnar nýju klasaeintaki af réttri sort
+function createShapeClass(x, y, col, lw, sh, filled, fo, tex) {
+ if(sh === 'brush') {
+  return new Brush(x, y, col, lw, sh, filled);
+ }
+ else if(sh === 'line') {
+  return new Line(x, y, col, lw, sh, filled);
+ }
+ else if(sh === 'square') {
+  return new Square(x, y, col, lw, sh, filled);
+ }
+ else if(sh === 'circle') {
+  return new Circle(x, y, col, lw, sh, filled);
+ }
+ else if(sh === 'triangle') {
+  return new Triangle(x, y, col, lw, sh, filled);
+ }
+ else if(sh === 'text') {
+  return new Texti(x, y, col, lw, sh, filled, fo, tex);
+ }
 }
 
 ////////////////////////////////////////////////
 //  KLASAR 
 ////////////////////////////////////////////////
 
-//GRUNNKLASINN
-var Shape = Base.extend({
-	constructor: function(x, y, col, lineW, shapeName, filled){
-		this.x = x;
-		this.y = y;
-        this.endx = x;
-        this.endy = y;
-		this.col = col;
-		this.lineW = lineW;
-        this.shapeName = shapeName;
-        this.filled = filled;
-        this.fo = fontur;
-        this.tex = texti;
-	},
+//  Shape er grunnklasinn sem aðrir klasar erfa frá.
+var Shape = Base.extend( {
+ constructor: function(x, y, col, lineW, shapeName, filled) {
+  this.x = x;
+  this.y = y;
+  this.endx = x;
+  this.endy = y;
+  this.col = col;
+  this.lineW = lineW;
+  this.shapeName = shapeName;
+  this.filled = filled;
+  this.fo = fontur;
+  this.tex = texti;
+ },
 });
 
-//Brush Class
-var Brush = Shape.extend({
-	constructor: function(x, y, col, lineW, shapeName, filled){
-		this.base(x, y, col, lineW, shapeName, filled);
-		this.hnitx = [];
-		this.hnity = [];
-		this.hnitx[0] = x;
-		this.hnity[0] = y;
-	},
+// Brush klasinn erfir frá Shape klasanum og teiknar pensil.
+var Brush = Shape.extend( {
+ constructor: function(x, y, col, lineW, shapeName, filled) {
+  this.base(x, y, col, lineW, shapeName, filled);
+  this.hnitx = [];
+  this.hnity = [];
+  this.hnitx[0] = x;
+  this.hnity[0] = y;
+ },
 
-	draw: function(ctx){
-		ctx.lineCap = "round";
-		ctx.lineWidth = this.lineW;
-		ctx.strokeStyle = this.col;
+ draw: function(ctx) {
+  ctx.lineCap = "round";
+  ctx.lineWidth = this.lineW;
+  ctx.strokeStyle = this.col;
+  for(var i = 0; i < this.hnitx.length; i++) {
+   this.drawlines(this.hnitx[i], this.hnity[i], 
+   this.hnitx[i+1], this.hnity[i+1]);
+  }
+ },
 
-		for(var i = 0; i < this.hnitx.length; i++){
-			this.drawlines(this.hnitx[i], this.hnity[i], 
-			this.hnitx[i+1], this.hnity[i+1]);
-			console.log(i + " " + this.hnitx[i]);
-		}
-	},
-
-	drawlines: function(fx,fy,tx,ty){
-		context.beginPath();
-		context.moveTo(fx,fy);
-		context.lineTo(tx,ty);
-		//context.closePath();
-		context.stroke();	
-	}
-	
+ drawlines: function(fx,fy,tx,ty) {
+  context.beginPath();
+  context.moveTo(fx,fy);
+  context.lineTo(tx,ty);
+  context.stroke();	
+ }
 });
 
-//Square Class
+//Square klasinn erfir frá Shape klasanum og teiknar ferhyrning.
 var Square = Shape.extend({
-	constructor: function(x, y, col, lineW, shapeName, filled){
-		this.base(x, y, col, lineW, shapeName, filled);
-        
-	},
+ constructor: function(x, y, col, lineW, shapeName, filled) {
+  this.base(x, y, col, lineW, shapeName, filled);
+ },
 
-	draw: function(ctx){
-		ctx.filled = this.filled;
-        ctx.x = this.x;
-		ctx.y = this.y;
-		ctx.strokeStyle = this.col;
-		ctx.lineWidth = this.lineW;
-		ctx.beginPath();
-		ctx.moveTo(this.x, this.y);
-		ctx.lineTo(this.x, this.endy);
-		ctx.lineTo(this.endx, this.endy);
-		ctx.lineTo(this.endx, this.y);
-		ctx.closePath();
-		ctx.stroke();
-		if(ctx.filled){
-			ctx.fillStyle = this.col;
-			ctx.fill();
-		}	
-	}
+ draw: function(ctx) {
+  ctx.filled = this.filled;
+  ctx.x = this.x;
+  ctx.y = this.y;
+  ctx.strokeStyle = this.col;
+  ctx.lineWidth = this.lineW;
+  ctx.beginPath();
+  ctx.moveTo(this.x, this.y);
+  ctx.lineTo(this.x, this.endy);
+  ctx.lineTo(this.endx, this.endy);
+  ctx.lineTo(this.endx, this.y);
+  ctx.closePath();
+  ctx.stroke();
+  if(ctx.filled) {
+   ctx.fillStyle = this.col;
+   ctx.fill();
+  }
+ }
 });
 
-//Line Class
+//Line klasinn erfir frá Shape klasanum og teiknar línu.
 var Line = Shape.extend({
-	constructor: function(x, y, col, lineW, shapeName, filled){
-		this.base(x, y, col, lineW, shapeName, filled);
-	},
+ constructor: function(x, y, col, lineW, shapeName, filled) {
+  this.base(x, y, col, lineW, shapeName, filled);
+ },
 
-	draw: function(ctx){
-        ctx.x = this.x;
-		ctx.y = this.y;
-		ctx.lineWidth = this.lineW;
-		ctx.strokeStyle = this.col;
-		ctx.beginPath();
-		ctx.moveTo(this.x, this.y);
-		ctx.lineTo(this.endx, this.endy);
-		ctx.closePath();
-		ctx.stroke();
-	}
+ draw: function(ctx) {
+  ctx.x = this.x;
+  ctx.y = this.y;
+  ctx.lineWidth = this.lineW;
+  ctx.strokeStyle = this.col;
+  ctx.beginPath();
+  ctx.moveTo(this.x, this.y);
+  ctx.lineTo(this.endx, this.endy);
+  ctx.closePath();
+  ctx.stroke();
+ }
 });
 
-//Circle Class
+//Circle klasinn erfir frá Shape klasanum og teiknar hring.
 var Circle = Shape.extend({
-	constructor: function(x, y, col, lineW, shapeName, filled){
-		this.base(x, y, col, lineW, shapeName, filled);
-		this.width;
-		this.height;
-		this.ox;	// horizontal offset
-		this.oy;	// vertical offset
-		this.xm;	// x middle
-		this.ym;	// y middle
-		
-	},
+ constructor: function(x, y, col, lineW, shapeName, filled) {
+  this.base(x, y, col, lineW, shapeName, filled);
+  this.width;
+  this.height;
+  this.ox;// horizontal offset
+  this.oy;// vertical offset
+  this.xm;// x miðja
+  this.ym;// y miðja
+ },
 
-	draw: function(ctx){
-		ctx.lineWidth = this.lineW;
-		ctx.strokeStyle = this.col;		
-		ctx.beginPath();
-		ctx.moveTo(this.x, this.ym);
-		ctx.bezierCurveTo(this.x, this.ym - this.oy, this.xm - 
-		this.ox, this.y, this.xm, this.y);
-		ctx.bezierCurveTo(this.xm + this.ox, this.y, this.endx, 
-		this.ym - this.oy, this.endx, this.ym);
-		ctx.bezierCurveTo(this.endx, this.ym + this.oy, this.xm + 
-		this.ox, this.endy, this.xm, this.endy);
-		ctx.bezierCurveTo(this.xm - this.ox, this.endy, this.x, 
-		this.ym + this.oy, this.x, this.ym);
-		ctx.closePath();
-		ctx.stroke();
-		ctx.filled = this.filled;
-		if(ctx.filled){
-			ctx.fillStyle = this.col;
-			ctx.fill();
-		}		
-	},
-	
-	savecircle: function(){
-		this.width = this.endx - this.x;
-		this.height = this.endy - this.y;
-		this.ox = (this.width / 2) * kappa;
-		this.oy = (this.height / 2) * kappa;
-		this.xm = this.x + (this.width/2);
-		this.ym = this.y + (this.height/2);
-	}
+ draw: function(ctx) {
+  ctx.lineWidth = this.lineW;
+  ctx.strokeStyle = this.col;		
+  ctx.beginPath();
+  ctx.moveTo(this.x, this.ym);
+  ctx.bezierCurveTo(this.x, this.ym - this.oy, this.xm - 
+   this.ox, this.y, this.xm, this.y);
+  ctx.bezierCurveTo(this.xm + this.ox, this.y, this.endx, 
+   this.ym - this.oy, this.endx, this.ym);
+  ctx.bezierCurveTo(this.endx, this.ym + this.oy, this.xm + 
+   this.ox, this.endy, this.xm, this.endy);
+  ctx.bezierCurveTo(this.xm - this.ox, this.endy, this.x, 
+   this.ym + this.oy, this.x, this.ym);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.filled = this.filled;
+  if(ctx.filled) {
+   ctx.fillStyle = this.col;
+   ctx.fill();
+  }
+ },
+
+ savecircle: function() {
+  this.width = this.endx - this.x;
+  this.height = this.endy - this.y;
+  this.ox = (this.width / 2) * kappa;
+  this.oy = (this.height / 2) * kappa;
+  this.xm = this.x + (this.width/2);
+  this.ym = this.y + (this.height/2);
+ }
 });
 
-//Text Class
+// Text klasinn erfir frá Shape klasanum og teiknar stafi.
 var Texti = Shape.extend({
-	constructor: function(x, y, col, lineW, shapeName, filled, fo, tex){
-		this.base(x, y, col, lineW, shapeName, filled)
-	},
+ constructor: function(x, y, col, lineW, shapeName, filled, fo, tex) {
+  this.base(x, y, col, lineW, shapeName, filled)
+ },
 
-	draw: function(ctx){
-		ctx.fillStyle = this.col;
-  		ctx.font = (this.lineW*5)+ 'px ' + this.fo;
-  		ctx.fillText(this.tex, this.x, this.y);
-	}
+ draw: function(ctx) {
+  ctx.fillStyle = this.col;
+  ctx.font = (this.lineW*5)+ 'px ' + this.fo;
+  ctx.fillText(this.tex, this.x, this.y);
+ }
 });
 
-//Triangle Class
+//  Triangle klasinn erfir frá Shape klasanum og teiknar þríhyrning.
 var Triangle = Shape.extend({
-	constructor: function(x, y, col, lineW, shapeName, filled){
-		this.base(x, y, col, lineW, shapeName, filled);
-	},
+ constructor: function(x, y, col, lineW, shapeName, filled) {
+  this.base(x, y, col, lineW, shapeName, filled);
+ },
 
-	draw: function(ctx){
-		ctx.filled = this.filled;
-        ctx.x = this.x;
-		ctx.y = this.y;
-		ctx.strokeStyle = this.col;
-		ctx.lineWidth = this.lineW;
-		ctx.beginPath();
-		ctx.moveTo(this.x, this.y);
-		ctx.lineTo(this.endx, this.endy);
-		ctx.lineTo(this.x - (this.endx - this.x), this.endy);
-		ctx.closePath();
-		ctx.stroke();
-		if(ctx.filled){
-			ctx.fillStyle = this.col;
-			ctx.fill();
-		}	
-	}
+ draw: function(ctx) {
+  ctx.filled = this.filled;
+  ctx.x = this.x;
+  ctx.y = this.y;
+  ctx.strokeStyle = this.col;
+  ctx.lineWidth = this.lineW;
+  ctx.beginPath();
+  ctx.moveTo(this.x, this.y);
+  ctx.lineTo(this.endx, this.endy);
+  ctx.lineTo(this.x - (this.endx - this.x), this.endy);
+  ctx.closePath();
+  ctx.stroke();
+  if(ctx.filled) {
+   ctx.fillStyle = this.col;
+   ctx.fill();
+  }
+ }
 });
