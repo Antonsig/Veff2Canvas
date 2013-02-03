e8f70e8cb5883813fa449dd2ba89128c39446765
Anton Sigurðsson <anton.sigurdsson@gmail.com>
2013-02-03 22:03:17 +0000

    Comment og JSLint

--- canvas3.js
+++ canvas3.js
@@ -1,3 +1,12 @@
+////////////////// Verkefni í Vefforritun2 HR - HTML5 Canvas ////////////////
+//  Nemendur:
+//   Anton Sigurðsson - antons11@ru.is
+//   Sigurður Nónsson - sigurdurjon11@ru.is
+/////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+//  GLOBAL BREYTUR
+////////////////////////////////////////////////
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 var mouseDown = false;
@@ -15,129 +24,128 @@ var mouseX = 0;
 var mouseY = 0;
 var mousedownX = 0;
 var mousedownY = 0;
-var kappa = .5522848; // Fyrir circle
+var kappa = 0.5522848;// Til að hægt sé að gera elypsulaga hring
 var isclean = false;
 
-function redraw(){
-    context.clearRect(0,0,800,400);
-	
-    for(var i = 0; i < shapeArraylength + 1; i++){		
-        shapeArray[i].draw(context);
-        console.log(shapeArray[i].x + " " + shapeArray[i].y + " " + shapeArray[i].endx + " " + shapeArray[i].endy + " " + shapeArray[i].lineW + " " + shapeArray[i].col + " " + shapeArray[i].shapeName);
-    }
+"use strict";
+////////////////////////////////////////////////
+//  GLOBAL FÖLL
+////////////////////////////////////////////////
+
+//  Teiknar aftur öll klasaeintök á canvasinn.
+function redraw() {
+ context.clearRect(0, 0, 800, 400);
+
+ for(var i = 0; i < shapeArraylength + 1; i++) {		
+  shapeArray[i].draw(context);
+  console.log(shapeArray[i].x + " " + shapeArray[i].y + " " 
+  + shapeArray[i].endx + " " + shapeArray[i].endy + " " 
+  + shapeArray[i].lineW + " " + shapeArray[i].col + " " 
+  + shapeArray[i].shapeName);
+ }
 }
 
-function undo(){
-	if(isclean){
-		var temp = undoRedoArraylength;
-		for(var i = 0; i < temp + 1; i++){
-			redo();
-		}
-
-	}
-	else{
-		if(shapeArraylength > -1){
-			undoRedoArray[undoRedoArraylength] = shapeArray[shapeArraylength];
-			undoRedoArraylength++;
-			shapeArraylength--;
-			var shapeArray2 = [];
-			for(var i = 0; i < shapeArraylength +1; i++){
-			shapeArray2[i] = shapeArray[i];
-			}
-			shapeArray = shapeArray2;
-			redraw();
-		}
-	}
-	isclean = false;	
-}
-
-function hreinsa(){
-		var len = shapeArraylength;
-		for (var i = 0; i < len +1; i++){
-			console.log("shapearraylength er " + shapeArraylength);
-			console.log(i);
-			undo();			
-		}
-		isclean = true;
-}
-
-function redo(){
-	if(undoRedoArraylength > 0){
-		undoRedoArraylength--;
-		shapeArraylength++;
-		shapeArray[shapeArraylength] = undoRedoArray[undoRedoArraylength];
-		redraw();
-	}
-}
-
-//til að stilla af mús ef padding og border trufla
-var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
-	if (document.defaultView && document.defaultView.getComputedStyle) {
-	stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
-	stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
-	styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
-	styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
-} 
-
-//Leið til að láta mús virka bæði í firefox og chrome
-getMouse = function(e, canvas) { 
-	var element = canvas, offsetX = 0, offsetY = 0;
-	if (element.offsetParent) {
-		do {
-			offsetX += element.offsetLeft;
-			offsetY += element.offsetTop;
-		} while ((element = element.offsetParent));
-	}
-
-	// Add padding and border style widths to offset
-	offsetX += stylePaddingLeft;
-	offsetY += stylePaddingTop;
-	offsetX += styleBorderLeft;
-	offsetY += styleBorderTop;
-
-	// We return an javascript object with x and y defined
-	return {
-		x: e.pageX - offsetX,
-		y: e.pageY - offsetY
-	};
-}
-
-function selcol(c){
-	color = c;
-}
-
-function selform(newForm){
-	form = newForm;
-}
-
-$('#buttonsave').click( function(){
-  var thecanvase = document.getElementById('myCanvas');
-  window.open(thecanvase.toDataURL('imge/png'));
-});
-
-$('#submit').click(function(){
-  var a = document.getElementById('inputbutton'),
-  b = encodeURI(a.value),
-  c = b.replace("C:%5Cfakepath%5C","");
-  var uploadcanvas = document.getElementById('myCanvas');
-  var uploadcontext = uploadcanvas.getContext('2d');
-  var img = new Image()
-  img.src = c
-  uploadcontext.drawImage(img,0,0);
-  if(a.value === ""){
-    alert('No image selected, press "Choose File" to select an image!');
+//  Tekur út síðasta klasaeintakið og vistar svo hægt sé að endurheimta.
+//  Einnig endurheimtir undo hreinsaðan canvas.
+function undo() {
+ if(isclean){
+  var temp = undoRedoArraylength;
+  for(var i = 0; i < temp + 1; i++) {
+  redo();
   }
+ }
+ else{
+  if(shapeArraylength > -1) {
+   undoRedoArray[undoRedoArraylength] = shapeArray[shapeArraylength];
+   undoRedoArraylength++;
+   shapeArraylength--;
+   var shapeArray2 = [];
+   for(var i = 0; i < shapeArraylength +1; i++) {
+    shapeArray2[i] = shapeArray[i];
+   }
+   shapeArray = shapeArray2;
+   redraw();
+  }
+ }
+ isclean = false;
+}
+
+//  Sækir síðasta undo-aða klasaeintak á canvasinn
+function redo(){
+ if(undoRedoArraylength > 0) {
+  undoRedoArraylength--;
+  shapeArraylength++;
+  shapeArray[shapeArraylength] = undoRedoArray[undoRedoArraylength];
+  redraw();
+ }
+}
+
+//  Hreinsar canvasinn með því að setja öll klasaeintök í undo
+function hreinsa() {
+ var len = shapeArraylength;
+ for (var i = 0; i < len +1; i++){
+  undo();			
+ }
+ isclean = true;
+}
+
+//  Til að láta mús virka rétt í öllum vöfrum
+getMouse = function(e, canvas) {
+ var element = canvas, offsetX = 0, offsetY = 0;
+ if (element.offsetParent) {
+  do{
+   offsetX += element.offsetLeft;
+   offsetY += element.offsetTop;
+  }while ((element = element.offsetParent));
+ }
+ return {
+  x: e.pageX - offsetX,
+  y: e.pageY - offsetY
+ };
+}
+
+//  Uppfærir valinn lit frá vefsíðu
+function selcol(c){
+ color = c;
+}
+
+//  Uppfærir valið form frá vefsíðu
+function selform(newForm){
+ form = newForm;
+}
+
+//  Vistar útlit canvasins og opnar í öðrum flipa
+$('#buttonsave').click( function() {
+ var thecanvase = document.getElementById('myCanvas');
+ window.open(thecanvase.toDataURL('imge/png'));
 });
 
+//  Sendir inn mynd á canvasinn 
+$('#submit').click(function() {
+ var a = document.getElementById('inputbutton'),
+ b = encodeURI(a.value),
+ c = b.replace("C:%5Cfakepath%5C","");
+ var uploadcanvas = document.getElementById('myCanvas');
+ var uploadcontext = uploadcanvas.getContext('2d');
+ var img = new Image()
+ img.src = c
+ uploadcontext.drawImage(img,0,0);
+ if(a.value === ""){
+  alert('No image selected, press "Choose File" to select an image!');
+ }
+});
+
+//  Sækir úr checkboxi hvort teikna eigi fylltan hlut
 $('#check').click( function(){
  if(document.getElementById('check').checked){
- 		fyllir = true;
- 	}
- 	else{
- 		fyllir = false;
- 	}
- });
+  fyllir = true;
+ }
+ else{
+  fyllir = false;
+ }
+});
 
+//  Mousedown skilar
 $('#myCanvas').mousemove(function (e){
 	if(mouseDown){
 		var tempMouse = getMouse(e, canvas);
@@ -169,8 +177,11 @@ $('#myCanvas').mousedown(function (e){
 	lineWidth = document.getElementById('linewidth').value;
 	fontur = document.getElementById('fontval').value;
 	texti = document.getElementById('textareitur').value;
-	var newShape = createShapeClass(mousedownX, mousedownY, color, lineWidth, form, fyllir, fontur, texti);
-    console.log("MX " + mousedownX + "| MY " + mousedownY + "| Color " + color + "| lineWidth " + lineWidth  + "| nameofShape " + form + "| Filled " + fyllir);
+	var newShape = createShapeClass(mousedownX, mousedownY, color, 
+	lineWidth, form, fyllir, fontur, texti);
+    console.log("MX " + mousedownX + "| MY " + mousedownY + "| Color " 
+	+ color + "| lineWidth " + lineWidth  + "| nameofShape " + form 
+	+ "| Filled " + fyllir);
     console.log(newShape.shapeName);
 	shapeArray.push(newShape);
 	mouseDown = true;
@@ -208,6 +219,10 @@ function createShapeClass(x, y, col, lw, sh, filled, fo, tex){
 	}
 }
 
+////////////////////////////////////////////////
+//  KLASAR 
+////////////////////////////////////////////////
+
 //GRUNNKLASINN
 var Shape = Base.extend({
 	constructor: function(x, y, col, lineW, shapeName, filled){
@@ -240,7 +255,8 @@ var Brush = Shape.extend({
 		ctx.strokeStyle = this.col;
 
 		for(var i = 0; i < this.hnitx.length; i++){
-			this.drawlines(this.hnitx[i], this.hnity[i], this.hnitx[i+1], this.hnity[i+1]);
+			this.drawlines(this.hnitx[i], this.hnity[i], 
+			this.hnitx[i+1], this.hnity[i+1]);
 			console.log(i + " " + this.hnitx[i]);
 		}
 	},
@@ -319,10 +335,14 @@ var Circle = Shape.extend({
 		ctx.strokeStyle = this.col;		
 		ctx.beginPath();
 		ctx.moveTo(this.x, this.ym);
-		ctx.bezierCurveTo(this.x, this.ym - this.oy, this.xm - this.ox, this.y, this.xm, this.y);
-		ctx.bezierCurveTo(this.xm + this.ox, this.y, this.endx, this.ym - this.oy, this.endx, this.ym);
-		ctx.bezierCurveTo(this.endx, this.ym + this.oy, this.xm + this.ox, this.endy, this.xm, this.endy);
-		ctx.bezierCurveTo(this.xm - this.ox, this.endy, this.x, this.ym + this.oy, this.x, this.ym);
+		ctx.bezierCurveTo(this.x, this.ym - this.oy, this.xm - 
+		this.ox, this.y, this.xm, this.y);
+		ctx.bezierCurveTo(this.xm + this.ox, this.y, this.endx, 
+		this.ym - this.oy, this.endx, this.ym);
+		ctx.bezierCurveTo(this.endx, this.ym + this.oy, this.xm + 
+		this.ox, this.endy, this.xm, this.endy);
+		ctx.bezierCurveTo(this.xm - this.ox, this.endy, this.x, 
+		this.ym + this.oy, this.x, this.ym);
 		ctx.closePath();
 		ctx.stroke();
 		ctx.filled = this.filled;
